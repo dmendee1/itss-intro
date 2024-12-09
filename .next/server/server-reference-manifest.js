777dfcfe4ff15cfbe808d3e2a3e34754aa6fa43d
dmendee1 <mendeemendee9@gmail.com/>
2024-12-10 07:30:20 +0800

    A

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"SVCQk2iUv0TS1FDPBSTknfs3tY/7glDEl7dk0FXUGDM=\"}"
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ap25vSJqhk/pD3v1RTJD39b8hz3gC/sBoD/+glJuNuA=\"\n}"
